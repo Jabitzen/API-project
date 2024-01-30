8d73915360b9a16bec0b37cacf76f8703968737b
joel abitzen <joel.abitzen@gmail.com>
2024-01-30 14:58:01 -0600

    add model associations

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -11,6 +11,27 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Spot.hasMany(models.Review, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+        hooks: true
+      })
+
+      Spot.belongsTo(models.User, {
+        foreignKey: 'ownerId'
+      })
+
+      Spot.hasMany(models.SpotImage, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+        hooks: true
+      })
+
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+        hooks: true
+      })
     }
   }
   Spot.init({
