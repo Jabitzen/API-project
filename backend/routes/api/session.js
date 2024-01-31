9027753ab3c6e2e30e0d68fcb47d8a7ab7e450c5
joel abitzen <joel.abitzen@gmail.com>
2024-01-30 18:33:56 -0600

    update session get route to include first/last name

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -78,6 +78,8 @@ router.post(
           id: user.id,
           email: user.email,
           username: user.username,
+          firstName: user.firstName,
+          lastName: user.lastName
         };
         return res.json({
           user: safeUser
